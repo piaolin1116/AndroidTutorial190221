ce75ce3d4d11027b2e2b8b396129d8affce580ca
Your Name <you@example.com>
2019-02-21 17:49:41 +0900

    broadCastReceiver

--- broadCastReceiver/app/src/main/java/com/example/a/broadcastreceiver/MainActivity.java
+++ broadCastReceiver/app/src/main/java/com/example/a/broadcastreceiver/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
                 int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
                 Toast.makeText(context, "level : " + level, Toast.LENGTH_SHORT).show();
                 Log.d("battery level","level : " + level);
+            }else if(intent.getAction().equals(Intent.ACTION_BATTERY_LOW)){
+
+            }else if(intent.getAction().equals("abc")){
+
             }
         }
     };
@@ -27,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSend = findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction("abc");
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
@@ -34,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        intentFilter.addAction(Intent.ACTION_BATTERY_LOW);
+        intentFilter.addAction("abc");
         registerReceiver(broadcastReceiver, intentFilter);
     }
 
