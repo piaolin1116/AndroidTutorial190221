4b0ddbad07cea6bbe884cb938707d81f8279642c
Your Name <you@example.com>
2019-02-21 14:36:32 +0900

    Thread

--- Thread/app/src/main/java/com/example/a/thread/MainActivity.java
+++ Thread/app/src/main/java/com/example/a/thread/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.a.thread;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,11 +10,18 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnStart;
+
     class MyThread extends Thread{
         @Override
         public void run() {
             for(int i=0; i<100; i++) {
                 Log.d("count", "count : " + i);
+                //btnStart.setText("count : " + i);
+                Message msg = handler.obtainMessage();
+                msg.what = 1;
+                msg.arg1 = i;
+                handler.sendMessage(msg);
                 try {
                     sleep(100);
                 }catch(Exception e){
@@ -22,12 +31,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == 1){
+                btnStart.setText("count : " + msg.arg1);
+            }
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnStart = findViewById(R.id.btnStart);
+        btnStart = findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
