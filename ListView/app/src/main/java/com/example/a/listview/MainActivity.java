993fda0389540b7fc50c6d7136fa4e3d291926dc
Your Name <you@example.com>
2019-02-21 13:40:43 +0900

    custom listivew

--- ListView/app/src/main/java/com/example/a/listview/MainActivity.java
+++ ListView/app/src/main/java/com/example/a/listview/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.a.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +22,11 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1 ,myData);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, myData[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
