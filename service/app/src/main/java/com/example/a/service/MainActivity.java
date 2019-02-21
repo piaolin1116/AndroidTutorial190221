e69650473f60906ad290e1178f71199bbf883185
Your Name <you@example.com>
2019-02-21 16:28:31 +0900

    service

---
+++ service/app/src/main/java/com/example/a/service/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.a.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnStart).setOnClickListener(this);
+        findViewById(R.id.btnStop).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnStart){
+            Toast.makeText(this, "btnStart", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MyService.class);
+            startService(intent);
+        }else if(v.getId() == R.id.btnStop){
+            Toast.makeText(this, "btnStop", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MyService.class);
+            stopService(intent);
+        }
+    }
+}
---
+++ service/app/src/main/java/com/example/a/service/MyService.java
@@ -0,0 +1,35 @@
+package com.example.a.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("service()","onCreate()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("service()","onDestroy()");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("service()","onStartCommand()");
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
