6b591d489fefb069ae65e62c6a3b08fd16e7bf7d
Your Name <you@example.com>
2019-02-26 18:07:04 +0900

    drag ans draw

---
+++ DragAndDraw/app/src/main/java/com/example/a/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,26 @@
+package com.example.a.draganddraw;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DragAndDrawFragment extends Fragment {
+
+
+    public static Fragment newInstance() {
+        return new DragAndDrawFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_drag_and_draw,container,false);
+        return v;
+    }
+}
---
+++ DragAndDraw/app/src/main/java/com/example/a/draganddraw/MyCheckBox.java
@@ -0,0 +1,23 @@
+package com.example.a.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class MyCheckBox extends RelativeLayout {
+    public MyCheckBox(Context context) {
+        super(context);
+    }
+
+    public MyCheckBox(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyCheckBox(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MyCheckBox(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
